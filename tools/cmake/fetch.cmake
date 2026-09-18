4850e90598e47a935a112330df7ee62faf8f218c
Thomas Spader <thomas.spader@gmail.com>
2026-09-18 03:52:27 -0400

    F

--- tools/cmake/fetch.cmake
+++ tools/cmake/fetch.cmake
@@ -1,7 +1,7 @@
 cmake_minimum_required(VERSION 3.24)
 
 set(DEPS
-  "sp|https://github.com/tspader/sp.git|1001a3c90b199a12e29c144a7eb6bc043386b732"
+  "sp|https://github.com/tspader/sp.git|36a2176e82c1f8da27bc94584e98d213caa9d6ad"
   "toml|https://github.com/tspader/toml.git|9b5f922de150c9eb6c4c7de28cae3c244633d9f1"
   "argparse|https://github.com/tspader/argparse.git|f71ed6c7b11cdbe75ffa0b42170530cc8610cbbf"
   "yyjson|https://github.com/ibireme/yyjson.git|ad58f21bee1213a8fdd614c2a11b4453815a73e9"
